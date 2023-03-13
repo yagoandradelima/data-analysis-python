30e7b31ebcc206ee1d86bad515a6740a6dd7b92d
Yago Andrade <yagoandradelima@outlook.com>
2023-03-13 18:57:17 -0300

    update padrão

--- analises/car_prices.py
+++ analises/car_prices.py
@@ -2,5 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv('C:/Users/yandrade/Documents/GitHub/data-analysis-python/databases')
+dataset = pd.read_csv('C:/Users/yandrade/OneDrive - Sá Cavalcante/Documentos/GitHub/data-analysis-python/databases/car_price.csv')
 print(dataset)
+
+size = np.random ....
+
+plt.scatter(dataset['wheelbase'], dataset['carlenght'], s=)
