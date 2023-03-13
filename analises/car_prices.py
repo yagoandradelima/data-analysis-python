1eaa40e2e7985784888e71529c6ea2c12f0a3001
Yago Andrade <yagoandradelima@outlook.com>
2023-03-13 17:48:06 -0300

    Create car_prices.py

---
+++ analises/car_prices.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('C:/Users/yandrade/Documents/GitHub/data-analysis-python/databases')
+print(dataset)
