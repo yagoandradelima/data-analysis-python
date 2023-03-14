386d39a229722e5b7bf33eb0031387b26559d4f6
Yago Andrade <yagoandradelima@outlook.com>
2023-03-14 16:09:19 -0300

    Update car_prices.py

--- analises/car_prices.py
+++ analises/car_prices.py
@@ -2,11 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-plt.style.use('_mpl-gallery')
+#plt.style.use('_mpl-gallery')
+
+#As análises nesse código não ocorrerão em formato de subplot, são apenas testes de visual
 
 #Realizando uma visualização analítica entre o comprimento e os eixos dos carros na lista
 dataset = pd.read_csv('databases/car_price.csv')
 plt.scatter(dataset['wheelbase'], dataset['carlength'], s=25)
 plt.xlabel('Distância entre eixos')
 plt.ylabel('Comprimento')
+plt.grid()
 plt.show()
+
+
+
+plt.pie(dataset['fueltype'].value_counts(), radius=2)
+plt
+plt.show()
+
