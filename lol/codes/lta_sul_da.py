a3ef41406617e8fc7f30dc5049154d661961d96e
Yago Lima Andrade <yagoandradelima@outlook.com>
2025-08-18 18:56:50 -0300

    Update lta_sul_da.py
    Criação das linhas de plot original assim como aplicações dos ensinamentos do livro de storytelling com indicação de cores. Iniciando a remoção de saturação.

--- lol/codes/lta_sul_da.py
+++ lol/codes/lta_sul_da.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import timedelta
+from matplotlib.ticker import FuncFormatter
 
 df = pd.read_csv("../data/match_data_2025.csv")
 
@@ -37,11 +38,15 @@ df_lta["truegamelength"]
 df_teams = df_lta[df_lta["position"] == "team"]
 
 df_teams
+
 # %%
+
 df_teams = df_teams[df_teams["split"] == "Split 3"]
+
 # %%
 
 df_teams
+
 # %%
 
 df_graphic = df_teams.groupby("teamname").agg({
@@ -55,11 +60,48 @@ df_graphic = df_teams.groupby("teamname").agg({
 # %%
 
 df_graphic["truegamelength"] = [timedelta(seconds=i) for i in df_graphic["gamelength"]]
+
 # %%
 
 df_graphic["truegamelength"] = df_graphic["truegamelength"].apply(f_timedelta)
 
+# %%
+
+df_graphic
 
 # %%
-df_graphic
+
+def y_formatter(y, pos):
+    return f'{x/1000:.0f}K' if x >= 1000 else str(x)
+
+# %%
+
+x = [10, 15, 20, 25]
+
+fig, ax = plt.subplots()
+
+for idx in df_graphic.index:
+
+    if idx == "paiN Gaming":
+        y = df_graphic.loc[idx][1:5]
+        ax.plot(x, y, color='red', linewidth=1)
+
+    elif idx == "FURIA":
+        y = df_graphic.loc[idx][1:5]
+        ax.plot(x, y, color='k', linewidth=1)
+
+    else:
+        y = df_graphic.loc[idx][1:5]
+        ax.plot(x, y, color='gray', linewidth=0.5)
+
+ax.yaxis.set_major_formatter(FuncFormatter(y_formatter))
+ax.set_title("Ouro por tempo")
+ax.set_xlabel("Minutos")
+ax.set_ylabel("Ouro (K)")
+ax.set_xticks([10, 15, 20, 25])
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+
+plt.show()
+
 # %%
