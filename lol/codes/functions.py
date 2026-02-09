82e3f0e931588b0f1365e3180b81c9e49e1ab02d
Yago Andrade <100331672+yagoandradelima@users.noreply.github.com>
2026-02-09 16:07:35 -0300

    Modificação de estrutura
    otimização de código e teste de funcionalidade

--- lol/codes/functions.py
+++ lol/codes/functions.py
@@ -14,60 +14,28 @@ def player_stats(player:str, data:pd.DataFrame):
     adjusted_deaths = player_data["deaths"].clip(lower=1)
     player_data["kda"] = (player_data["kills"]+player_data["assists"])/adjusted_deaths
 
+    # criando as listas de colunas para acesso futuro
+    colunas_desempenho_jg = ["kda","monsterkills","xpdiffat10","xpdiffat15","xpdiffat20","xpdiffat25","golddiffat10","golddiffat15","golddiffat20","golddiffat25","wardsplaced","wpm","wardskilled","wcpm","controlwardsbought","visionscore","vspm"]
+
     if player_data["position"].unique() == "jng":
-
+        
         qtd_partidas = player_data.shape[0]
-        team = player_data["teamname"].unique()
-        liga = player_data["league"].unique()
-        qtd_pool = player_data["champion"].unique().shape[0]
-        top_picks = [x for x in player_data["champion"].value_counts().nlargest(5).index]
-        qtd_match_top_picks = player_data["champion"].value_counts().nlargest(5).values
-        kda_mean = round(player_data["kda"].mean(),2)
-        cs_jungle = round(player_data["monsterkills"].mean(),2)
-        xp_diff_10 = round(player_data["xpdiffat10"].mean(),2)
-        xp_diff_15 = round(player_data["xpdiffat15"].mean(),2)
-        xp_diff_20 = round(player_data["xpdiffat20"].mean(),2)
-        xp_diff_25 = round(player_data["xpdiffat25"].mean(),2)
-        gold_diff_10 = round(player_data["golddiffat10"].mean(),2)
-        gold_diff_15 = round(player_data["golddiffat15"].mean(),2)
-        gold_diff_20 = round(player_data["golddiffat20"].mean(),2)
-        gold_diff_25 = round(player_data["golddiffat25"].mean(),2)
-        wardsplaced = round(player_data["wardsplaced"].mean(),2)
-        wards_per_minute = round(player_data["wpm"].mean(),2)
-        wards_killed = round(player_data["wardskilled"].mean(),2)
-        ward_control_placed_per_minute = round(player_data["wcpm"].mean(),2)
-        ward_control_bought = round(player_data["controlwardsbought"].mean(),2)
-        visionscore = round(player_data["visionscore"].mean(),2)
-        visionscore_per_minute = round(player_data["vspm"].mean(),2)
-
+    
+        for column in player_data.columns:
+            if column in ['playername', 'teamname', 'league']:
+                print(f"{column}: {player_data[column].unique()}")
+                
         print(
             f"""
-            Jogador: {player}\n
-            Equipe: {team}\n
-            Liga: {liga}\n
-            Quantidade de partidas: {qtd_partidas}\n
-            Quantidade de pool: {qtd_pool}\n
-            Campeões mais usados: {top_picks}\n
-            Quantidade de partidas jogadas com os campeões: {qtd_match_top_picks}\n
-            KDA médio por partida: {kda_mean}\n
-            Média CS jungle: {cs_jungle}\n
-            Média de Wards posicionadas: {wardsplaced}\n
-            Média de Wards posicionadas por minuto: {wards_per_minute}\n
-            Média de Wards removidas: {wards_killed}\n
-            Média de Ward de controle posicionadas por minuto: {ward_control_placed_per_minute}\n
-            Média de Wards de controle compradas: {ward_control_bought}\n
-            Média de Placar de visão: {visionscore}\n
-            Média de Placar de visão por minuto: {visionscore_per_minute}\n
-            Média XP Diff 10m: {xp_diff_10}\n
-            Média XP Diff 15m: {xp_diff_15}\n
-            Média XP Diff 20m: {xp_diff_20}\n
-            Média XP Diff 25m: {xp_diff_25}\n
-            Média Gold Diff 10m: {gold_diff_10}\n
-            Média Gold Diff 15m: {gold_diff_15}\n
-            Média Gold Diff 20m: {gold_diff_20}\n
-            Média Gold Diff 25m: {gold_diff_25}\n
+            Pool: {player_data["champion"].unique().shape[0]}\n
+            Top picks: {[x for x in player_data["champion"].value_counts().nlargest(5).index]}\n
+            Partidas jogadas com os Top Picks: {player_data["champion"].value_counts().nlargest(5).values}\n
             """
         )
+
+        for column in player_data.columns:
+            if column in [colunas_desempenho_jg]:
+                print(f"{column}: {round(player_data[column].mean()),2}")
 
         player_data_top_champs = player_data[player_data["champion"].isin(top_picks)]
     
